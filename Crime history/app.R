0ed12321e312692c0fce99c515cacf4f78f2e736
Chris Werner-Westoby <56901476+chris-ww@users.noreply.github.com>
2020-03-31 17:43:02 -0400

    working dashboard

---
+++ Crime history/app.R
@@ -0,0 +1,57 @@
+library(shiny)
+
+historydf<-read.csv("history.csv")
+
+df<-historydf %>%
+  group_by(year,desc1)%>%
+  summarize(n()) %>%
+  filter(year>2006) 
+
+names(df)<-c("year","desc","count")
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Crimes by year"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      selectInput(inputId = "crime",
+                  label = "Crime:",choices=unique(df$desc),selected="BURGLARY")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    newdf=df[df$desc==input$crime,]
+    print(newdf)
+    plot(newdf$year,newdf$count, main="number of crimes per year",xlab="year",ylab="count",xlim=c(2006,2017),type="l")
+    
+  })
+  
+}
+shinyApp(ui = ui, server = server)
--- import.R
+++ import.R
@@ -1,1 +1,21 @@
+library(shiny)
+library(tidyverse)
+
+
 crimedf<-read.csv("NYDP_crime_data.csv")
+
+names(crimedf)<-c("id","start_date","start_time","end_date","end_time","precinct","report_date","code1","desc1","code2","desc2","success","level","borough","loc","loc_type","jur","jur_code","park","hdev","dev_code","x_loc","y_loc","s_age","s_race","s_sex","transit_district","latitude","longitude","lat_lon","patrol_burough","station_name","v_age","v_race","v_sex")
+
+historydf<-crimedf[,c("start_date","start_time","borough","desc1")]
+historydf$year=format(as.Date(historydf$start_date, format="%m/%d/%Y"),"%Y")
+write.csv(historydf,"Crime History/history.csv")
+
+
+
+historydf %>%
+  group_by(year,borough,desc1)%>%
+  summarize(n()) %>%
+  filter(year>2006)
+runApp("Crime History")
+
+unique(historydf$year)
